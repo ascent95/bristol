efcc7cfe5f76f0c3584f3220978b6b507e348e01
Matthew Duffin <matthewjduffin@gmail.com>
2015-02-01 16:56:28 +0000

    Now on 90 points. Regex is working.

--- java/Grade.java
+++ java/Grade.java
@@ -1,5 +1,24 @@
 class Grade
 {
+    private static String getGradeBinary(int mark) throws NotMarkException
+    {
+    
+        int[] gb = {0, 40, 50, 60, 70, 80, 90, 100, 101};
+        String[] grades =
+        {
+            "Fail",
+            "Third Class",
+            "Lower Second Class",
+            "Upper Second Class",
+            "First Class",
+            "Above and Beyond",
+            "Publishable",
+            "Perfect"
+        };
+        //System.out.format("mark: %d\n", mark);
+        return grades[bSearch(gb,mark,0,gb.length-1)];
+    }
+    
     private static String getGrade(int mark)
     {
         if (mark == 100) return "Perfect";
@@ -11,24 +30,49 @@ class Grade
         else if (mark >= 40) return "Third Class";
         else return "Fail";
     }
+    
+    private static int bSearch(int[] gb, int key, int min, int max) throws NotMarkException
+    {
+        if(max < min)
+            throw new NotMarkException("Key not in array.");
+        else
+        {
+            int mid = (min+max)/2;
+            System.out.format("Split: %d\n", mid);
+            if (gb[mid] > key)
+                return bSearch(gb,key,min,mid-1);
+            else if (gb[mid] <= key && gb[mid+1] > key)
+                return mid;
+            else
+                return bSearch(gb,key,mid+1,max);
+        }
+    }
 
     public static void main(String[] args)
-    {
+    {        
         try
         {
             double acc = 0;
             double mark;
+            boolean bin = false;
+            boolean flags = true;
+            int argNo = 0;
             for(int i=0; i<args.length; i++)
             {
-                if(args[i].matches("^[0-9]+\\.?[0-9]$")
+                if (flags && args[i].equals("-binary"))
+                    bin = true;
+                else if(args[i].matches("([1-9]\\d*|0)\\.?[0-9]?"))
                 {
+                    flags = false;
+                    argNo++;
                     mark = Double.parseDouble(args[i]);
                     if (mark < 0 || mark > 100) throw new NotMarkException("Not in the range from 0 to 100.");
                     else acc += mark;
                 }
-                else throw new NotMarkException("Not a number with one decimal place in the range 0 to 100.");            
+                else throw new NotMarkException("Not a number with zero or one decimal places.");            
             }
-            System.out.println(getGrade((int)Math.ceil((double)acc/args.length)));
+            if (bin) System.out.println(getGradeBinary((int)Math.round(acc/argNo)));
+            else System.out.println(getGrade((int)Math.round(acc/argNo)));
         } 
         catch(NotMarkException ex)
         {
